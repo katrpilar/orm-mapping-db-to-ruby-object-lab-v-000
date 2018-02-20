57059de6f60e1719c5185c4423b91da786b57ff3
Katrina Brinson <kbrinso@gmail.com>
2018-02-20 00:01:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,4 +71,13 @@ class Student
     
     DB[:conn].execute(sql)
   end
+  
+  def students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
 end
