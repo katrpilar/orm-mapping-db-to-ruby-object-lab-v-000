9de4c6d127d623ce59e8078163dbfcf28226ff83
Katrina Brinson <kbrinso@gmail.com>
2018-02-20 00:00:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,5 +63,12 @@ class Student
     DB[:conn].execute(sql)
   end
   
-
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
 end
