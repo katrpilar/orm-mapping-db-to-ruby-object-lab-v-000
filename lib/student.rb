659b1fca95d19b428069ad1f76397be41b5de139
Katrina Brinson <kbrinso@gmail.com>
2018-02-20 00:13:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -112,4 +112,15 @@ class Student
     }.first
     # self.first_X_students_in_grade_10(1)
   end
+  
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+    SQL
+    
+     DB[:conn].execute(sql,grade).map{|row|
+      self.new_from_db(row)
+    }
+  end
 end
